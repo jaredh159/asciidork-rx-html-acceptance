17f98e23d26f6002c78150e057b63f5944b7905d
Jared Henderson <jaredh159@users.noreply.github.com>
2025-03-24 08:08:22 -0400

    reorg tests, prepare for ci usage

--- check.js
+++ check.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+// @ts-check
 
 const fs = require(`node:fs`);
 const path = require(`node:path`);
@@ -6,35 +7,54 @@ const { execSync } = require(`node:child_process`);
 
 /**
  * @param {string} dir - The starting directory to walk.
+ * @param {string} bin - The path to the asciidork binary.
+ * @param {boolean} verbose - Whether to output verbose messages.
  */
-function processAdocFiles(dir) {
+function processAdocFiles(dir, bin, verbose) {
   const entries = fs.readdirSync(dir, { withFileTypes: true });
 
   for (const entry of entries) {
     const fullpath = path.join(dir, entry.name);
     if (entry.isDirectory()) {
-      processAdocFiles(fullpath);
+      processAdocFiles(fullpath, bin, verbose);
     } else if (entry.isFile() && entry.name.endsWith(`.adoc`)) {
       const adocPath = fullpath;
       const htmlPath = fullpath.replace(/input\.adoc$/, `output.html`);
 
-      process.stdout.write(`Checking ${fullpath}...\n`);
+      if (verbose) {
+        process.stdout.write(`Checking ${fullpath}...\n`);
+      }
       try {
         // test w/ windows line endings
         // execSync(`/usr/bin/perl -i -pe 's/\\n/\\r\\n/' ${adocPath}`);
-        const html = execSync(
-          `/Users/jared/jaredh159/asciidork/target/debug/asciidork -i ${adocPath} -s unsafe -e`,
-          { encoding: `utf8` }
-        );
+        const html = execSync(`${bin} -i ${adocPath} -s unsafe -e`, {
+          encoding: `utf8`,
+        });
         fs.writeFileSync(htmlPath, html, { encoding: `utf8` });
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(`Failed to process ${fullpath}:`, error.message);
+      } catch (err) {
+        process.stderr.write(`Failed to process ${fullpath}: ${err.message}\n`);
       }
     }
   }
 }
 
-processAdocFiles(__dirname);
+const verbose = process.argv.includes(`--verbose`);
+const asciidorkBin = process.argv[2];
+if (!asciidorkBin) {
+  process.stderr.write(`asciidork binary path not passed\n`);
+  process.exit(1);
+}
 
-execSync(`prettier --write ${__dirname}/**/*.html`, { encoding: `utf8` });
+processAdocFiles(__dirname, asciidorkBin, verbose);
+
+execSync(
+  `${__dirname}/node_modules/.bin/prettier --write ${__dirname}/**/*.html`,
+  { encoding: `utf8` }
+);
+
+const gitStatus = execSync(`git status --porcelain tests`, {
+  cwd: __dirname,
+  encoding: `utf8`,
+});
+
+process.exit(gitStatus.length === 0 ? 0 : 1);
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Asciidork RX HTML5 Backend Acceptance Test Suite
+
+Usage:
+
+```sh
+$ pnpm install
+$ node ./check.js /path/to/asciidork/bin # --verbose
+# if git status is clean, pass
+```
